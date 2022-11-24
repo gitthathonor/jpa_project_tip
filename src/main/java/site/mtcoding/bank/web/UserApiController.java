b38a08b1b9fc0fa1f2670e075f15ee0a5eba6675
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-24 14:37:22 +0900

    로그인 테스트 완료

--- src/main/java/site/mtcoding/bank/web/UserApiController.java
+++ src/main/java/site/mtcoding/bank/web/UserApiController.java
@@ -2,12 +2,15 @@ package site.mtcoding.bank.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import site.mtcoding.bank.config.auth.LoginUser;
 import site.mtcoding.bank.dto.ResponseDto;
 import site.mtcoding.bank.dto.UserReqDto.JoinReqDto;
 import site.mtcoding.bank.dto.UserRespDto.JoinRespDto;
@@ -19,6 +22,11 @@ import site.mtcoding.bank.service.UserService;
 public class UserApiController {
 
     private final UserService userService;
+
+    @GetMapping("/user/session")
+    public String userSession(@AuthenticationPrincipal LoginUser loginUser) {
+        return "username : " + loginUser.getUsername();
+    }
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody JoinReqDto joinReqDto) {
